4baa129d367e1d7dde6b0552f39449a3fb1b0098
glo1337 <vacys1337@gmail.com>
2025-04-11 19:32:47 +0500

    Заготовка конвертации значений пользователя

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -6,12 +6,22 @@ const USD_TO_RUB float64 = 83.74
 const USD_TO_EUR float64 = 1.137
 
 func main() {
-	eur := 2.0
-	res := EurToRub(eur)
-	fmt.Println(res)
+	// eur := 2.0
+	// res := EurToRub(eur)
+	// fmt.Println(res)
 }
 
 func EurToRub(value float64) float64 {
 	usd := value * USD_TO_EUR
 	return usd * USD_TO_RUB
+}
+
+func scanInput(message string) (value string) {
+	fmt.Println(message)
+	fmt.Scanln(&value)
+	return value
+}
+
+func convert(value float64, from string, to string) float64 {
+	return 0
 }
